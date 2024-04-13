e558d12bb8f93be3c1ad4a324568ff6a4a211eaf
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-13 17:10:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.getElementById("myButton").addEventListener("click", function() {
-  document.getElementById("myText").innerHTML = "Привет, мир!";
+document.getElementById("loanForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var loanAmount = parseFloat(document.getElementById("loanAmount").value);
+    var loanTerm = parseInt(document.getElementById("loanTerm").value);
+    var interestRate = parseFloat(document.getElementById("interestRate").value);
+
+    var monthlyInterestRate = (interestRate / 100) / 12;
+    var loanTermMonths = loanTerm * 12;
+    var numerator = loanAmount * monthlyInterestRate;
+    var denominator = 1 - Math.pow(1 + monthlyInterestRate, -loanTermMonths);
+    var monthlyPayment = numerator / denominator;
+
+    document.getElementById("monthlyPayment").innerText = "Ежемесячный платеж: " + monthlyPayment.toFixed(2) + " тенге";
 });
