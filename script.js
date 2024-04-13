7db0748fd3e7efad88e12de94c991e31a554f672
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-13 18:54:32 +0500

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Форматирование ввода суммы кредита
 document.getElementById("loanAmount").addEventListener("input", function(event) {
-    var loanAmount = event.target.value.replace(/\D/g, "");
+    var loanAmount = event.target.value.replace(/[^\d.]/g, ""); // Убираем все символы кроме цифр и точки
     event.target.value = numberWithCommas(loanAmount);
 });
 
 // Форматирование ввода процентной ставки
 document.getElementById("interestRate").addEventListener("input", function(event) {
-    // Оставляем процентную ставку без форматирования
+    var interestRate = event.target.value.replace(/[^\d.]/g, ""); // Убираем все символы кроме цифр и точки
+    event.target.value = interestRate;
 });
