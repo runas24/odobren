6d4aa1f94ee05bee78f877f4f3fde834e81235f6
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-13 18:52:13 +0500

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Функция для добавления разделителя в числа для удобочитаемости
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
 // Форматирование ввода суммы кредита
 document.getElementById("loanAmount").addEventListener("input", function(event) {
     var loanAmount = event.target.value.replace(/\D/g, "");
@@ -11,6 +6,5 @@ document.getElementById("loanAmount").addEventListener("input", function(event) 
 
 // Форматирование ввода процентной ставки
 document.getElementById("interestRate").addEventListener("input", function(event) {
-    var interestRate = event.target.value.replace(/\D/g, "");
-    event.target.value = numberWithCommas(interestRate);
+    // Оставляем процентную ставку без форматирования
 });
